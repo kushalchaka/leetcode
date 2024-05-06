2b20f2dd4b3ab9f6e49b7ee0575fbd13ca3f670a
kushalchaka@gmail.com <kushalchaka@gmail.com>
2024-05-05 19:58:27 -0500

    hashmap

---
+++ java/containsduplicate.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashMap <Integer, Integer> result = new HashMap <Integer, Integer>();
+        int temp = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (result.get(nums[i]) == null) {
+                result.put(nums[i], temp);
+            }
+            else
+            {
+                return true;
+            }
+            
+        }
+        return false;
+    }
+    
+}
